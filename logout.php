c25fff7d5fc0c286fd4d13538fdc53b61d63d3e0
Arif Siddik Muharam <89333963+arifsiddikm@users.noreply.github.com>
2023-01-21 18:06:58 +0700

    updated

--- logout.php
+++ logout.php
@@ -1,8 +1,8 @@
-<?php  
+<?php   
 session_start();
 session_destroy();
 echo "<script>
         alert('Logout berhasil, selamat tinggal ^_^');
         document.location.href='login.php';
         </script>";
-?>
+?> 
